5822bb84ac726077209b0e07373ec638799e2758
jungai <juworaphol125@gmail.com>
2023-12-22 12:31:45 +0700

    test fetch data from raw github

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { cn } from "./lib/utils";
-import { ref } from "vue";
+import { ref, onMounted, computed } from "vue";
 import {
   Tabs,
   TabTrigger,
@@ -8,7 +8,7 @@ import {
   TabIndicator,
   TabContent,
 } from "@ark-ui/vue";
-import { ChevronLeft, ChevronRight } from "lucide-vue-next";
+import { ChevronLeft, ChevronRight, MapPin } from "lucide-vue-next";
 
 type Items = {
   data: {
@@ -27,167 +27,17 @@ type Items = {
   }[];
 };
 
-const items: Items = {
-  data: [
-    {
-      date: "Dec,21",
-      missions: [
-        {
-          startTime: "10.30AM",
-          endTIme: "10.40AM",
-          totalDistance: 3.46,
-          missioId: "DOH-TEST-SMALL",
-          station: "PTIC",
-          flightId: "DOH-FL-23001",
-          memo: "บินทดสอบ DOH & ML",
-          totalTime: 7,
-          status: "done",
-        },
-        {
-          startTime: "11.25AM",
-          endTIme: "11.32AM",
-          totalDistance: 3.46,
-          missioId: "DOH-TEST-SMALL",
-          station: "PTIC",
-          flightId: "DOH-FL-23001",
-          memo: "บินทดสอบ DOH & ML",
-          totalTime: 7,
-          status: "next",
-        },
-        // {
-        //   startTime: "12.00PM",
-        //   endTIme: "12.20PM",
-        //   totalDistance: 7.43,
-        //   missioId: "FR-S55700-3RVL",
-        //   station: "ทล.1 55+700",
-        //   flightId: "DOH-FL-23003",
-        //   memo: "บินสำรวจ จุดเปิดเลนสวน ถ.พหลโยธิน ช่วง กม.54+550 ถึง กม.58+250",
-        //   totalTime: 20,
-        //   status: "waiting",
-        // },
-        // {
-        //   startTime: "13.30PM",
-        //   endTIme: "13.58PM",
-        //   totalDistance: 12.82,
-        //   missioId: "FR-S71500-2RVL",
-        //   station: "ทล.1 71+500",
-        //   flightId: "DOH-FL-23004",
-        //   memo: "บินสํารวจ จุดเปิดเลนสวน ถ.พหลโยธิน กม. 71+200 ถึง กม. 77+000 ",
-        //   totalTime: 28,
-        //   status: "waiting",
-        // },
-      ],
-    },
-    // {
-    //   date: "Mon,29",
-    //   missions: [
-    //     {
-    //       startTime: "9.00AM",
-    //       endTIme: "9.25AM",
-    //       totalDistance: 14,
-    //       missioId: "FR-S55700-48750",
-    //       station: "ทล.1 55+700",
-    //       flightId: "DOH-FL-23001",
-    //       memo: "บินสำรวจทาง ถ.พหโยธิน ขาเข้า ระหว่าง กม.55+700 ถึง กม.48+750",
-    //       totalTime: 14,
-    //       status: "waiting",
-    //     },
-    //     {
-    //       startTime: "9.00AM",
-    //       endTIme: "9.25AM",
-    //       totalDistance: 14,
-    //       missioId: "FR-S55700-48750",
-    //       station: "ทล.1 55+700",
-    //       flightId: "DOH-FL-23001",
-    //       memo: "บินสำรวจทาง ถ.พหโยธิน ขาเข้า ระหว่าง กม.55+700 ถึง กม.48+750",
-    //       totalTime: 14,
-    //       status: "waiting",
-    //     },
-    //     {
-    //       startTime: "9.00AM",
-    //       endTIme: "9.25AM",
-    //       totalDistance: 14,
-    //       missioId: "FR-S55700-48750",
-    //       station: "ทล.1 55+700",
-    //       flightId: "DOH-FL-23001",
-    //       memo: "บินสำรวจทาง ถ.พหโยธิน ขาเข้า ระหว่าง กม.55+700 ถึง กม.48+750",
-    //       totalTime: 14,
-    //       status: "waiting",
-    //     },
-    //     {
-    //       startTime: "9.00AM",
-    //       endTIme: "9.25AM",
-    //       totalDistance: 14,
-    //       missioId: "FR-S55700-48750",
-    //       station: "ทล.1 55+700",
-    //       flightId: "DOH-FL-23001",
-    //       memo: "บินสำรวจทาง ถ.พหโยธิน ขาเข้า ระหว่าง กม.55+700 ถึง กม.48+750",
-    //       totalTime: 14,
-    //       status: "waiting",
-    //     },
-    //   ],
-    // },
-    // {
-    //   date: "Mon,30",
-    //   missions: [
-    //     {
-    //       startTime: "9.00AM",
-    //       endTIme: "9.25AM",
-    //       totalDistance: 14,
-    //       missioId: "FR-S55700-48750",
-    //       station: "ทล.1 55+700",
-    //       flightId: "DOH-FL-23001",
-    //       memo: "บินสำรวจทาง ถ.พหโยธิน ขาเข้า ระหว่าง กม.55+700 ถึง กม.48+750",
-    //       totalTime: 14,
-    //       status: "waiting",
-    //     },
-    //     {
-    //       startTime: "9.00AM",
-    //       endTIme: "9.25AM",
-    //       totalDistance: 14,
-    //       missioId: "FR-S55700-48750",
-    //       station: "ทล.1 55+700",
-    //       flightId: "DOH-FL-23001",
-    //       memo: "บินสำรวจทาง ถ.พหโยธิน ขาเข้า ระหว่าง กม.55+700 ถึง กม.48+750",
-    //       totalTime: 14,
-    //       status: "waiting",
-    //     },
-    //     {
-    //       startTime: "9.00AM",
-    //       endTIme: "9.25AM",
-    //       totalDistance: 14,
-    //       missioId: "FR-S55700-48750",
-    //       station: "ทล.1 55+700",
-    //       flightId: "DOH-FL-23001",
-    //       memo: "บินสำรวจทาง ถ.พหโยธิน ขาเข้า ระหว่าง กม.55+700 ถึง กม.48+750",
-    //       totalTime: 14,
-    //       status: "waiting",
-    //     },
-    //     {
-    //       startTime: "9.00AM",
-    //       endTIme: "9.25AM",
-    //       totalDistance: 14,
-    //       missioId: "FR-S55700-48750",
-    //       station: "ทล.1 55+700",
-    //       flightId: "DOH-FL-23001",
-    //       memo: "บินสำรวจทาง ถ.พหโยธิน ขาเข้า ระหว่าง กม.55+700 ถึง กม.48+750",
-    //       totalTime: 14,
-    //       status: "waiting",
-    //     },
-    //   ],
-    // },
-  ],
-};
+const items = ref<Items>({ data: [] });
 
-const value = ref(items.data[0].date);
+const value = computed(() => items.value?.data[0]?.date ?? "");
 
-// onMounted(async () => {
-//   const raw = await fetch(
-//     "https://raw.githubusercontent.com/jungai/dotfiles/master/editor/.prettierrc.json",
-//   );
-//   const data = await raw.json();
-//   console.log(data);
-// });
+onMounted(async () => {
+  const raw = await fetch(
+    "https://raw.githubusercontent.com/jungai/doh-mission-plan/master/data.json",
+  );
+  const data = await raw.json();
+  items.value = data;
+});
 </script>
 
 <template>
